ee18548f86e715ef51dcb4d8a1791a2d3dd9d6cb
RailanderJ <36487179+RailanderJoseSantos@users.noreply.github.com>
2023-11-15 22:18:29 -0300

    Adicionei validação no formulario checkListBox, no txtbox nome do veiculo para que somente primeira letra em maiusculo e não permitir nomes duplicados

--- frmCheckedListBox.Designer.cs
+++ frmCheckedListBox.Designer.cs
@@ -28,21 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.chklbTransportes = new System.Windows.Forms.CheckedListBox();
+            this.btnMostrarSelecionado = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnResetar = new System.Windows.Forms.Button();
+            this.btnNovoTransporte = new System.Windows.Forms.Button();
+            this.txbNovoTransp = new System.Windows.Forms.TextBox();
+            this.lblDigiteNomeTransp = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // chklbTransportes
+            // 
+            this.chklbTransportes.FormattingEnabled = true;
+            this.chklbTransportes.Items.AddRange(new object[] {
+            "Carro",
+            "Avião",
+            "Navio",
+            "Ônibus",
+            "Trêm"});
+            this.chklbTransportes.Location = new System.Drawing.Point(12, 12);
+            this.chklbTransportes.Name = "chklbTransportes";
+            this.chklbTransportes.Size = new System.Drawing.Size(121, 409);
+            this.chklbTransportes.TabIndex = 0;
+            // 
+            // btnMostrarSelecionado
+            // 
+            this.btnMostrarSelecionado.Location = new System.Drawing.Point(152, 11);
+            this.btnMostrarSelecionado.Name = "btnMostrarSelecionado";
+            this.btnMostrarSelecionado.Size = new System.Drawing.Size(146, 30);
+            this.btnMostrarSelecionado.TabIndex = 1;
+            this.btnMostrarSelecionado.Text = "Mostrar Selecionados";
+            this.btnMostrarSelecionado.UseVisualStyleBackColor = true;
+            this.btnMostrarSelecionado.Click += new System.EventHandler(this.btnMostrarSelecionado_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(152, 47);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(146, 30);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar Lista";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnResetar
+            // 
+            this.btnResetar.Location = new System.Drawing.Point(152, 83);
+            this.btnResetar.Name = "btnResetar";
+            this.btnResetar.Size = new System.Drawing.Size(146, 30);
+            this.btnResetar.TabIndex = 3;
+            this.btnResetar.Text = "Resetar Lista";
+            this.btnResetar.UseVisualStyleBackColor = true;
+            this.btnResetar.Click += new System.EventHandler(this.btnResetar_Click);
+            // 
+            // btnNovoTransporte
+            // 
+            this.btnNovoTransporte.Location = new System.Drawing.Point(152, 391);
+            this.btnNovoTransporte.Name = "btnNovoTransporte";
+            this.btnNovoTransporte.Size = new System.Drawing.Size(146, 30);
+            this.btnNovoTransporte.TabIndex = 4;
+            this.btnNovoTransporte.Text = "Adicionar novo transporte";
+            this.btnNovoTransporte.UseVisualStyleBackColor = true;
+            this.btnNovoTransporte.Click += new System.EventHandler(this.btnNovoTransporte_Click);
+            // 
+            // txbNovoTransp
+            // 
+            this.txbNovoTransp.Location = new System.Drawing.Point(152, 365);
+            this.txbNovoTransp.Name = "txbNovoTransp";
+            this.txbNovoTransp.Size = new System.Drawing.Size(145, 20);
+            this.txbNovoTransp.TabIndex = 5;
+            this.txbNovoTransp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbNovoTransp_KeyPress);
+            // 
+            // lblDigiteNomeTransp
+            // 
+            this.lblDigiteNomeTransp.AutoSize = true;
+            this.lblDigiteNomeTransp.Location = new System.Drawing.Point(149, 339);
+            this.lblDigiteNomeTransp.Name = "lblDigiteNomeTransp";
+            this.lblDigiteNomeTransp.Size = new System.Drawing.Size(131, 13);
+            this.lblDigiteNomeTransp.TabIndex = 6;
+            this.lblDigiteNomeTransp.Text = "Digite um novo transporte:";
             // 
             // frmCheckedListBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(335, 450);
+            this.Controls.Add(this.lblDigiteNomeTransp);
+            this.Controls.Add(this.txbNovoTransp);
+            this.Controls.Add(this.btnNovoTransporte);
+            this.Controls.Add(this.btnResetar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnMostrarSelecionado);
+            this.Controls.Add(this.chklbTransportes);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmCheckedListBox";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CheckedListBox";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox chklbTransportes;
+        private System.Windows.Forms.Button btnMostrarSelecionado;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnResetar;
+        private System.Windows.Forms.Button btnNovoTransporte;
+        private System.Windows.Forms.TextBox txbNovoTransp;
+        private System.Windows.Forms.Label lblDigiteNomeTransp;
     }
 }
--- frmCheckedListBox.cs
+++ frmCheckedListBox.cs
@@ -12,9 +12,123 @@ namespace Components
 {
     public partial class frmCheckedListBox : Form
     {
+        protected string statusLista="";
+        protected int contaLetras = 0;
         public frmCheckedListBox()
         {
             InitializeComponent();
+        }
+
+        private void btnMostrarSelecionado_Click(object sender, EventArgs e)
+        {
+            string selecionados = "";
+            foreach(string s in chklbTransportes.CheckedItems)//percorre todos selecionados
+            {
+                selecionados += s+", ";
+            }
+            if (selecionados.Equals(""))
+            {
+                selecionados = "Nenhum checkbox selecionado!";
+            }
+            MessageBox.Show(selecionados);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //verifica se ja esta limpo
+            if (! statusLista.Equals("limpar"))
+            {
+                chklbTransportes.Items.Clear();//limpa todos os checkedbox (os checkbox somem)
+                statusLista = "limpar";//informa limpada, assim pode ser resetada se preciso
+            }
+            else
+            {
+                MessageBox.Show("Não há o que limpar!");
+            }
+        }
+
+        private void btnResetar_Click(object sender, EventArgs e)
+        {
+            /*o resetar deve voltar todos os itens padroes*/
+
+            //evita duplo reset e ficar repetido
+            if (! statusLista.Equals("resetar"))
+            {
+                /*object carro = "Carro";
+                object aviao = "Avião";
+                object navio = "Navio";
+                object onibus = "Ônibus";
+                object trem = "Trêm";
+                chklbTransportes.Items.Add(carro, true);
+                chklbTransportes.Items.Add(aviao, true);
+                chklbTransportes.Items.Add(navio, true);
+                chklbTransportes.Items.Add(onibus, true);
+                chklbTransportes.Items.Add(trem, true);
+                /*/
+
+                //Alternativa usando lista
+                List<string> listaString = new List<string>();
+                listaString.Add("Carro");
+                listaString.Add("Avião");
+                listaString.Add("Navio");
+                listaString.Add("Ônibus");
+                listaString.Add("Trêm");
+
+                //colocando a lista inteira no checkeListbox
+                chklbTransportes.Items.AddRange(listaString.ToArray());
+
+                //informa ja resetada assim não pode resetar denovo antes do limpar
+                statusLista = "resetar";
+            }
+            else
+            {
+                MessageBox.Show("Não há o que resetar!");
+            }
+        }
+
+        private void btnNovoTransporte_Click(object sender, EventArgs e)
+        {
+            //se tiver algo escrito
+            if( !this.txbNovoTransp.Text.Equals(""))
+            {
+                if (chklbTransportes.Items.Contains(txbNovoTransp.Text) ){
+                    MessageBox.Show("Já tem um meio de transporte chamado", txbNovoTransp.Text);
+                    
+                }else
+                {
+                    chklbTransportes.Items.Add(txbNovoTransp.Text, false);
+                }
+            }
+            contaLetras =0;
+            txbNovoTransp.Text="";
+            btnNovoTransporte.Focus();
+        }
+
+        private void txbNovoTransp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Coloca em minusculo
+            if (char.IsUpper(e.KeyChar) ) {
+                e.KeyChar = char.ToLower(e.KeyChar);
+            }
+
+            //se digitou letra e foi primeira difitação
+            if(contaLetras==0)
+            {
+                if (char.IsLetter(e.KeyChar))
+                {
+                    //pegue o valor e trasnforme em maiusculo (passar para o txbox não precisa tratar)
+                    e.KeyChar = char.ToUpper(e.KeyChar);
+                    contaLetras++;
+                }
+            }else{
+                e.KeyChar = char.ToLower(e.KeyChar);
+            }
+            //Se não digitar letra zera contador pra evitar apagar e ficar primeira letra minuscula
+            if ( (char.IsLetter(e.KeyChar)==false) ){//se não for letra
+                if (char.IsNumber(e.KeyChar) == false) { //Se não for numero
+                    contaLetras++;//aumenta contador pra forçar minusculo
+                }
+            }
         }
     }
 }
